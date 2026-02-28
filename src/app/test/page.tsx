9a49095889ab792c55c45949d90f42ac3e468842
Divyanshu Singh Rajput <yashrajcg@gmail.com>
2026-02-28 17:07:36 +0530

    ad

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,12 +3,13 @@ import PhishingSandbox from '@/components/PhishingSandbox'
 import SquidGame from '@/components/SquidGame'
 import Chapter from '@/harsh/chapter'
 import CyberPuzzle from '@/harsh/CyberPuzzle'
+import QuizRunner from '@/yash/QuizRunner'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-    <SquidGame/>
+    <QuizRunner/>
     
     </div>
   )
